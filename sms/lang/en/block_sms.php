0c5d080cc27ce7cfde5744dea3b0d34d569b4192
mattmoodle <lombardi.mat@gmail.com>
2014-07-04 17:03:02 +1000

    first commit

---
+++ sms/lang/en/block_sms.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/* SMS Notifier Block
+ * SMS notification is a one way SMS messaging block that allows managers, teachers and administrators to
+ * send text messages to their student and teacher.
+ * @package blocks
+ * @author: Azmat Ullah, Talha Noor
+ * @date: 06-Jun-2013
+*/
+
+    $string['pluginname'] = 'SMS Notifier';
+    $string['sms'] = 'SMS Notifier';
+    $string['sms_send'] = 'Send SMS';
+    $string['addpage'] = 'Add Page';
+    $string['textfields'] = 'Data';
+    $string['edithtml'] = 'Data Manipulation';
+    $string['pagetitle'] = 'Input Text';
+    $string['picturedesc'] = 'Description';
+    $string['sms_save'] = 'Message Template';
+    $string['sms_history'] = 'Message History';
+    $string['selectcourse'] = 'Select Course';
+    $string['selectrole'] = 'Select Role';
+    $string['sms_body'] = 'Message Body';
+    $string['selectmsg'] = 'Select Message';
+    $string['template'] = 'Create Message Template';
+    $string['sms_template'] = 'Create Message Template';
+    $string['sms_template_header'] = 'New Template';
+    $string['sms_api_key'] = 'API Key';
+    $string['sms_api_username'] = 'Username';
+    $string['sms_api_password'] = 'Password';
+    $string['sms_setting_header'] = 'SMS Notification Settings';
+    $string['block_sms_apikey'] = 'block_sms_apikey';
+    $string['block_sms_api_username'] = 'block_sms_api_username';
+    $string['block_sms_api_password'] = 'block_sms_api_password';
+    $string['sms:addinstance']='Add Instance';
+    $string['error'] = 'Error';
+    $string['sent'] = 'Sent';
+    $string['valid_key'] = 'Your key IS VALID';
+    $string['serial_no'] = 'Serial No.';
+    $string['name'] = 'Name';
+    $string['msg_body'] = 'Message Body';
+    $string['edit'] = 'Edit';
+    $string['delete'] = 'Delete';
+    $string['cell_no'] = 'Cell Number';
+    $string['select'] = 'Select';
+    $string['askfordelete'] = 'Do You Want to Delete This Template?';
+    $string['moodleuser'] = 'Username';
+    $string['usernumber'] = 'Numbers';
+    $string['status'] = 'Status';
+
+    //ADDENDUM
+
+    $string['adminheaderapi'] = 'API settings';
+    $string['admindescapi'] = 'Set up the API for SMS messaging';
+    $string['adminheaderrolesettings'] = 'Role settings';
+    $string['admindescrolesettings'] = 'Define the users that can receive SMS';
+    $string['adminheaderrolefilter'] = 'Select roles allowed to receive SMS';
+    $string['descrolefilter'] = 'Select multiple roles holding CTRL key. Here roles are shown with their shortnames.';
+
+
+
+
+
+
+
